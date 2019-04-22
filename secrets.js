bb843d4ef86ba6d67554d13da770763d2b603528
Norka Avalos <norka.avalos11@gmail.com>
2019-04-22 17:28:10 -0400

    basic UI

---
+++ secrets.js
@@ -0,0 +1,9 @@
+const chatbotKey = 'wFv99LVMT3OHGuPhK3mXo2X9zczKa5iF8gutYuCHqZb7';
+const assistantIdPer = 'ef068c98-a382-495f-9d87-1938c67118c5';
+const eventBrite = 'V5GQ2EIUNVP32NONKGCH';
+
+module.exports = {
+  chatbotKey,
+  assistantIdPer,
+  eventBrite,
+};
