da7a4bef9d7fe2adf12e9778dfc871d7d4d0d3da
Norka Avalos <norka.avalos11@gmail.com>
2019-04-24 14:29:06 -0400

    feat: chatbot lines edited

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { TopPart } from './components/TopPart';
 import { Chat } from './components/Chat';
--- src/components/TopPart.js
+++ src/components/TopPart.js
@@ -4,14 +4,11 @@ export class TopPart extends Component {
 	render(){
 		return(
 			<div className="container-fluid">
-				<div className="navbar navbar-expand-lg narbar-light bg-light">
-					<h1 className="navbar-brand font-weight-bold">ThinkerBot</h1>
-				</div>
         <div className="jumbotron">
-          <h1 className="display-4">Des Formulas plsy humanins</h1>
-          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <h1 className="display-4">Hello, I am Tinkerbot</h1>
+          <p className="lead">I am a chatbot that will help you search and filter events on Eventbrite.</p>
           <hr className="my-4"/>
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p>Let's chat!</p>
         </div>
 			</div>
 		);
