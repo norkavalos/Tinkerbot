bd887de98bc5ddd970376332fc808ab414673d40
Norka Avalos <norka.avalos11@gmail.com>
2019-07-06 16:54:46 -0400

    new assistant

--- secrets.js
+++ secrets.js
@@ -1,5 +1,5 @@
 const chatbotKey = 'wFv99LVMT3OHGuPhK3mXo2X9zczKa5iF8gutYuCHqZb7';
-const assistantIdPer = 'ef068c98-a382-495f-9d87-1938c67118c5';
+const assistantIdPer = '699aacaa-86f4-4884-8e3f-e9d63c806ecc';
 const eventBrite = 'V5GQ2EIUNVP32NONKGCH';
 
 module.exports = {
@@ -7,3 +7,7 @@ module.exports = {
   assistantIdPer,
   eventBrite,
 };
+
+/*
+old asssistant id : ef068c98-a382-495f-9d87-1938c67118c5
+*/
