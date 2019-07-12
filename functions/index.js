ec798ac542c30d46bbb828e6eb800d7d62186c24
Cristian Avalos <cavalos@kargo.com>
2019-07-12 11:55:14 -0400

    reads keys

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,12 @@ const axios = require('axios');
 const path = require('path');
 const Assistant = require('watson-developer-cloud/assistant/v2');
 
+const keys = functions.config();
+const assistantId = keys.watson.assistant_id;
+const eventBrite_key = keys.eventbrite.key;
 // Set up assistant service wrapper
 const service =  new Assistant({
-  iam_apikey: keys.chatbotKey,
+  iam_apikey: keys.watson.key,
   version: '2018-09-20'
 });
 
